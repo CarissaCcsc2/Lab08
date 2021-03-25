4b8702d78707fe7cf3800d1d16c9e049b0710fd3
unknown <71133414+CarissaCcsc2@users.noreply.github.com>
2021-03-24 20:42:30 -0400

    updated

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,8 @@
-userName = input ("Hello! Please enter your name!")
+userName = input ("Hello! Please enter your Facebook username!")
 
-print ("Hello", userName, "nice to meet you")
+userPassword = input ("Hello! Please enter your Facebook password!")
+
+if(userName.lower() == "carissa" and userPassword == "hello"):
+    print("Welcome to Facebook!")
+else:
+    print("Wrong credentials, try again!")
